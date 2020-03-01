5aa7c10d3ad64468acdabcdb5a5972edac794683
alexdevteam <alexinfdev@gmail.com>
2020-03-01 21:57:10 +0100

    Add snowflake struct

---
+++ include/snowflake.hpp
@@ -0,0 +1,14 @@
+#ifndef DUALVENTER_SNOWFLAKE_HPP
+#define DUALVENTER_SNOWFLAKE_HPP
+
+#include <cstdint>
+
+namespace dualventer {
+
+struct Snowflake {
+  std::uint_fast64_t id;
+};
+
+}
+
+#endif // DUALVENTER_SNOWFLAKE_HPP
