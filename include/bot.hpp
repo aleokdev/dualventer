a2d9a7e1fd6e250be471905098a45baa6097e089
alexdevteam <alexinfdev@gmail.com>
2020-03-02 13:58:09 +0100

    Get heartbeats + WSS client working

--- include/bot.hpp
+++ include/bot.hpp
@@ -1,25 +1,28 @@
 #ifndef DUALVENTER_BOT_HPP
 #define DUALVENTER_BOT_HPP
 
-#include <client_ws.hpp>
+#include <client_wss.hpp>
 
 #include <future>
 #include <string_view>
 #include <string>
+#include <atomic>
 
 namespace dualventer {
 
 class Bot {
 public:
-  using Client = SimpleWeb::SocketClient<SimpleWeb::WS>;
+  using Client = SimpleWeb::SocketClient<SimpleWeb::WSS>;
 
   Bot(std::string token);
+  ~Bot();
 
   void send_message(std::string const&);
 
   const inline static std::string api_url{"https://discordapp.com/api/v6"};
 private:
-  bool connection_closed = false;
+  std::atomic<bool> connection_closed = false;
+  std::atomic<size_t> last_sequence_number = -1;
   std::thread heartbeat;
   Client client;
 
--- src/bot.cpp
+++ src/bot.cpp
@@ -1,43 +1,84 @@
 #include "bot.hpp"
 
-#include <client_ws.hpp>
+#include <chrono>
 #include <cpr/cpr.h>
+#include <future>
 #include <iostream>
 #include <rapidjson/document.h>
 #include <utility>
-#include <future>
-#include <chrono>
 
 namespace dualventer {
 
-Bot::Bot(std::string token) : token(std::move(token)), client(get_gateway_uri()) {
+Bot::Bot(std::string token)
+    : token(std::move(token)), client(get_gateway_uri()) {
   std::cout << "Gateway URI: " << get_gateway_uri() << std::endl;
   client.on_open = [](std::shared_ptr<Client::Connection> connection) {
     std::cout << "Client: Opened connection" << std::endl;
   };
-  client.on_error = [](std::shared_ptr<Client::Connection> /*connection*/, const SimpleWeb::error_code &ec) {
-    std::cout << "Client: Error: " << ec << ", error message: " << ec.message() << std::endl;
+  client.on_error = [](std::shared_ptr<Client::Connection> /*connection*/,
+                       const SimpleWeb::error_code &ec) {
+    std::cout << "Client: Error: " << ec << ", error message: " << ec.message()
+              << std::endl;
   };
-  client.on_message = [](std::shared_ptr<Client::Connection> connection, std::shared_ptr<Client::Message> message) {
-    std::cout << "Client: Message received: \"" << message->string() << "\"" << std::endl;
+  client.on_message = [this](std::shared_ptr<Client::Connection> connection,
+                             std::shared_ptr<Client::Message> message) {
+    std::string str_message = message->string();
+    std::cout << "Client: Message received: \"" << str_message << "\""
+              << std::endl;
+    rapidjson::Document doc;
+    doc.Parse(str_message.c_str());
+    if (doc["s"].GetType() == rapidjson::Type::kNullType)
+      last_sequence_number = -1;
+    else
+      last_sequence_number = doc["s"].GetUint64();
+    switch (doc["op"].GetUint()) {
+    // TODO: Reconnect if server doesn't respond to heartbeats with opcode 11
+    case 10: // Hello (handshake)
+    {
+      std::size_t heartbeat_interval =
+          doc["d"]["heartbeat_interval"].GetUint64();
+      heartbeat = std::thread([this, heartbeat_interval, connection]() {
+        for (;;) {
+          std::this_thread::sleep_for(
+              std::chrono::milliseconds(heartbeat_interval));
+          if (connection_closed)
+            return;
+          std::cout << "Sending heartbeat." << std::endl;
+          std::string hb_str(R"({"op":1,"d":)");
+          if (last_sequence_number == (std::size_t)-1)
+            hb_str += "null}";
+          else
+            hb_str += std::to_string(last_sequence_number) + "}";
+          auto send_stream = std::make_shared<Client::SendStream>();
+          *send_stream << hb_str;
+          connection->send(send_stream);
+        }
+      });
+      heartbeat.detach();
+    } break;
+    }
   };
   std::cout << "Starting client" << std::endl;
   client.start();
 }
 
-void Bot::send_message(std::string const & txt) {
-
-}
+void Bot::send_message(std::string const &txt) {}
 
 std::string Bot::get_gateway_uri() {
-  auto r = cpr::Get(cpr::Url{api_url+"/gateway"}, cpr::VerifySsl(false));
+  auto r = cpr::Get(cpr::Url{api_url + "/gateway"}, cpr::VerifySsl(false));
   std::cout << r.error.message << std::endl;
   std::cout << r.text << std::endl;
   rapidjson::Document doc;
   doc.Parse(r.text.c_str());
   std::string str(doc["url"].GetString());
-  str = str.substr(6) + "/?v=6&encoding=json"; // Remove "wss://" and specify API version and encoding to use
+  str = str.substr(6) + "/?v=6&encoding=json"; // Remove wss:// and specify API
+                                               // version and encoding to use
   return str;
 }
 
+Bot::~Bot() {
+  connection_closed = false;
+  std::cout << "Closing connection." << std::endl;
 }
+
+} // namespace dualventer
